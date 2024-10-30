f2c678a36a955d961806b1ffc61d2b9c95bb09d3
tcorzo <t.corzo.profesional@gmail.com>
2024-10-30 01:08:21 +0000

    feature: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'node:path';
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.url, "./src"),
-    },
-  },
+  base: '/abacus-emulator/',
 });
